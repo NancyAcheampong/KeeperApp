b5f0f4bbc60a3c0b6bc6dc35a470d0325bbc67d3
NancyAcheampong <nancyhanns1@gmail.com>
2025-08-15 02:06:37 +0000

    Added new features to Keeper App

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,13 @@
 
 function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
     return (
         <div className="note">
             <h1 className="title">{props.title}</h1>
             <p className="content">{props.content}</p>
+             <button onClick={handleClick}>DELETE</button>
         </div>
     )
 }
